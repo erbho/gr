c496c75351ae597489baa2711ef7fe24216edd88
Joe-kaiwai <107242033+Joe-kaiwai@users.noreply.github.com>
2022-07-07 21:30:51 +0800

    php create

---
+++ footer.php
@@ -0,0 +1,53 @@
+<footer>
+        <div class="container py-2">
+
+          <div class="row">
+
+            <div class="col-8 row pt-4">
+              <div class="col">
+                <h5 class="mb-4 fw-bold">關於我們</h5>
+                <ui class="footerUl">
+                  <li class="my-2"><a href="">租車</a></li>
+                  <li class="my-2"><a href="">租車流程</a></li>
+                  <li class="my-2"><a href="">最新優惠</a></li>
+                  <li class="my-2"><a href="">聯絡我們</a></li>
+                </ui>
+              </div>
+              <div class="col">
+                <h5 class="mb-4 fw-bold">內部專用</h5>
+                <ui class="footerUl">
+                  <li><a href="./staff-login.php">職員登入</a></li>
+                </ui>
+              </div>
+            </div>
+             
+            <div class="col-4 d-flex align-items-center">
+              <img src="./image/index-img/logo.png" alt="logo" width="100%">
+            </div>
+           
+          </div>
+        </div>  
+
+        
+        <div class="footer text-center">@2022 Copyright: GreenRental Company</div><br>
+        <div class="container">
+          <div class="socialMedia d-flex justify-content-center">
+            <a href="https://www.facebook.com/" class="mx-3"><i class="fa-brands fa-facebook fa-2x"></i></a>
+            <a href="https://www.instagram.com/" class="mx-3"><i class="fa-brands fa-instagram fa-2x" style="color:rgb(158, 86, 120) ;"></i></a>
+            <a href="https://twitter.com/" class="mx-3"><i class="fa-brands fa-twitter fa-2x" style="color:rgb(23, 134, 23);"></i></a>
+          </div>
+        </div>
+        
+        <div class="container text-end">
+          <a href="#">back to top</a>
+        </div>
+       
+        
+        </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ staff-login.php
@@ -0,0 +1,20 @@
+<?php include('head.php')?>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('footer.php')?>
