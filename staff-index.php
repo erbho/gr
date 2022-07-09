9d22e613ee4b3ddc3d24e66753a4ffac80ed6747
KW <kaiwaing1722@gmail.com>
2022-07-10 01:53:03 +0800

    staff-index, create order table

--- staff-index.php
+++ staff-index.php
@@ -1,20 +1,17 @@
 <?php include('header.php');
 
 if(!isset($_SESSION['name']))
-header("location: ./staff-login.php");
-?>
-
-<nav class="navbar container-fluid d-flex justify-content-between ms-2 mt-2">
-    <div class="navbar-brand staff-footer-companyName ">
+header("location: ./staff-login.php");?>
+<nav class="navbar container-fluid d-flex justify-content-between mt-2">
+    <div class="navbar-brand staff-footer-companyName">
       <img src="./image/index-img/round_logo.png" alt="green rental logo" class="staff-greenRentalLogo">
       <h2 class="fw-bolder d-inline-block" style="color: rgb(41, 119, 101)">Green-Rental CMS</h2>
     </div>
 
-  
     <div>
 
       <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" aria-controls="offcanvasExample" style="background-color:#fff;border: 0;">
-        <i class="fa-solid fa-circle-user fa-3x me-5" style="color:rgb(73, 73, 151)"></i>
+      <i class="fa-solid fa-circle-user fa-3x me-5" style="color:rgb(73, 73, 151)"></i>
       </a>
 
       <div class="offcanvas offcanvas-end rounded-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
@@ -52,9 +49,28 @@ header("location: ./staff-login.php");
 
 </nav><hr>
 
-<div class="d-flex align-items-start my-5 container-fluid">
+<!-- horizon nav -->
+<ul class="nav nav-pills mb-3 d-sm-none ms-3" id="v-pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Contact</button>
+  </li>
+</ul>
 
-  <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+
+
+
+
+<div class="d-flex align-items-start my-5 container-fluid flex-column flex-sm-row ">
+
+  <!-- vertical nav -->
+  <div class="nav flex-column nav-pills me-3 d-sm-flex d-none" id="v-pills-tab" role="tablist" aria-orientation="vertical">
     <button class="nav-link active my-3" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
       type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Customer Order</button>
     <button class="nav-link my-3" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
@@ -68,32 +84,60 @@ header("location: ./staff-login.php");
 
 
 
-  <div class="tab-content container-fluid" id="v-pills-tabContent">
+
+  <div class="tab-content container" id="v-pills-tabContent">
     <!--------- nav content-1 ----------->
     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"
-      tabindex="0">
-      <div class="card mb-3" style="max-width: 700px; margin: auto;">
-        <div class="row g-0">
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Order Number:</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.</p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
+        tabindex="0">
+
+        <div class="d-flex justify-content-start align-items-center">
+          <label for="searchOrderId" class="me-2">Search</label>
+          <input type="text" class="me-2" id="searchOrderId" placeholder="Order Number" style="width:120px">
+          <button>Submit</button>
+        </div>
+
+          <div class="card my-4">
+            
+          <div class="card-body table-responsive my-4">
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Order Number</th>
+                  <th scope="col">Customer Name</th>
+                  <th scope="col">Customer Phone</th>
+                  <th scope="col">Customer Email</th>
+                  <th scope="col">Pick-up Time</th>
+                  <th scope="col">Return Time</th>
+                  <th scope="col">Collect Location</th>
+                  <th scope="col">Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-      </div>
     </div>
+ 
+    
 
 
 
 
     <!--------- nav content-2 ----------->
     <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
-      ...</div>
+      2
+    </div>
 
 
 
@@ -102,8 +146,11 @@ header("location: ./staff-login.php");
 
     <!--------- nav content-3 ----------->
     <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"
-      tabindex="0">...</div>
+      tabindex="0">
+      3
+    </div>
   </div>
 </div>
+
 
 <?php include('staff-footer.php');?>
