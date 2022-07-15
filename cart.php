87b409998f37681032e4c74107e0617be274897a
Joe-KW <kaiwaing1722@gmail.com>
2022-07-15 12:39:33 +0800

    cart page echo date

--- cart.php
+++ cart.php
@@ -1,5 +1,10 @@
 <?php include("index-header.php");?>
-  <div class="bgColor"><br><br>
+
+<?php $carQ = mysqli_query($dbConnection, 'SELECT * from `car` where `car_id`='.$_GET['car_id'].'');
+$car = mysqli_fetch_assoc($carQ);
+?>
+
+  <div class="bgColor"><p><p>
         
           
         
@@ -36,81 +41,85 @@
         
               <div class="logo">
                   <img src="./image/index-img/round_logo.png" alt="green rental logo" class="greenRentalLogo">
-                </div>
+              </div>
         
                 <div class="container mt-5">
-                <table class="table table-bordered">
-                  <thead>
-                    <tr>
-                      <th class="m-0 py-3" scope="col"><h5>預訂車輛：</h5></th>
-                      <th class="m-0 py-3" scope="col" ><h5>日期：</h5></th>
-                      <th class="m-0 py-3" scope="col"><h5>價錢：</h5></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">
-                          <img src="./image/index-img/audi.png" width="300px"> 
-                          <p style="font-size: 20px;">Audi RS e-tron GT</p>                
-                      </th>
-                      <td>12/07/2022 - 16/07/2022</td>
-                      <td style ="text-align: right; line-height: 30px;">租車費：$3,090<br>按金：$10,000<br>附加項目：$0<br>優惠券：N/A<br>稅項：$0<br>手續費：$0</td>
-                    </tr>
+                  <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th class="m-0 py-3" scope="col"><h5>預訂車輛：</h5></th>
+                        <th class="m-0 py-3" scope="col" ><h5>日期：</h5></th>
+                        <th class="m-0 py-3" scope="col"><h5>價錢：</h5></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">
+                            <img src="./image/index-img/<?php echo $car['image'];?>" width="250"> 
+                            <p style="font-size: 20px;"><?php echo $car['name'];?>                
+                        </th>
+                        <td><input readonly class="form-control-plaintext" type="datetime-local" value="<?php echo $_GET['pickupTime'];?>"> - <input readonly class="form-control-plaintext" type="datetime-local" value="<?php echo $_GET['returnTime'];?>"></td>
+                        <td style ="text-align: right; line-height: 30px;">
+                          租車費：$<?php echo $car['price'];?>
+                          <br>按金：$10,000
+                          <br>附加項目：$0
+                          <br>優惠券：N/A
+                          <br>稅項：$0
+                          <br>手續費：$0
+                        </td>                       
+                      </tr>
+                      <tr>
+                        <th class="m-0 py-4" style="text-align:right" scope="row" colspan="3" > 
+                        <h5>合共：$13,090</h5>
+                        </th>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
 
-                    <tr>
-                      <th class="m-0 py-4" style="text-align:right" scope="row" colspan="3" > 
-                      <h5>合共：$13,090</h5>
-                      </th>
-                    </tr>
-                  </tbody>
-                  </table>
-              </div>
-
-                <div class="container py-3 w-75 my-5" style="border: 1px solid rgb(233, 231, 231); background-color: rgba(218, 220, 220,1); border-radius: 15px;">
+              <div class="container py-3 w-75 my-5" style="border: 1px solid rgb(233, 231, 231); background-color: rgba(218, 220, 220,1); border-radius: 15px;">
                   
                   
-                  <form class="w-75" style="margin:auto" action="./function.php?op=createOrder" method="post">
-
-                <h5 class="m-0 py-4">個人資料：</h5>
-                  <div class="row">
-                    <div class=" col form-floating mb-4">
-                      <input type="Name" class="form-control" id="floatingInput" placeholder="姓名" required>
-                      <label class="ms-2" for="floatingInput">姓名：</label>
-                    </div>
-                    <div class="col form-floating mb-4">
-                      <input  type="Phone" class="form-control" id="floatingInput" placeholder="電話" required>
-                      <label class="ms-2" for="floatingInput">電話：</label>
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-8 form-floating mb-4">
-                      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-                      <label class="ms-2" for="floatingInput">Email：</label>
-                    </div>
-                    <div class="col form-floating mb-4">
-                      <input type="IDno" class="form-control" id="floatingInput" placeholder="身份證號碼" required>
-                      <label class="ms-2" for="floatingInput">身份證號碼：</label>
-                    </div>
-                  </div>
-
-                    <div class="form-floating">
-                      <input style="height: 310px; margin-bottom: 20px;" type="Remark" class="form-control" id="floatingInput" placeholder="備註">
-                      <label for="floatingPassword">備註：</label>
-                    </div>
-
-                    
-                    
-                    <div class="container d-flex justify-content-end">
-                      <button style="margin: 10px; width:100px;" type="reset" class="btn btn-primary">重設</button>
-                      <button style="margin: 10px ;width:100px;" type="submit" class="btn btn-dark nextbutton">確認訂單</button>
-                  
+                <form class="w-75" style="margin:auto" action="./function.php?op=createOrder" method="post">
+                  <h5 class="m-0 py-4">個人資料：</h5>
+                      <div class="row">
+                        <div class=" col form-floating mb-4">
+                          <input type="Name" class="form-control" id="floatingInput" placeholder="姓名" required>
+                          <label class="ms-2" for="floatingInput">姓名：</label>
+                        </div>
+                        <div class="col form-floating mb-4">
+                          <input  type="Phone" class="form-control" id="floatingInput" placeholder="電話" required>
+                          <label class="ms-2" for="floatingInput">電話：</label>
+                        </div>
                       </div>
-                  </form>
 
-                  </div>
+                      <div class="row">
+                        <div class="col-8 form-floating mb-4">
+                          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                          <label class="ms-2" for="floatingInput">Email：</label>
+                        </div>
+                        <div class="col form-floating mb-4">
+                          <input type="IDno" class="form-control" id="floatingInput" placeholder="身份證號碼" required>
+                          <label class="ms-2" for="floatingInput">身份證號碼：</label>
+                        </div>
+                      </div>
 
-                  <?php include("index-footer.php")?>
+                      <div class="form-floating">
+                        <input style="height: 310px; margin-bottom: 20px;" type="Remark" class="form-control" id="floatingInput" placeholder="備註">
+                        <label for="floatingPassword">備註：</label>
+                        </div>
+
+                        
+                        
+                        <div class="container d-flex justify-content-end">
+                          <button style="margin: 10px; width:100px;" type="reset" class="btn btn-primary">重設</button>
+                          <button style="margin: 10px ;width:100px;" type="submit" class="btn btn-dark nextbutton">確認訂單</button> 
+                        </div>
+                </form>
+
+               </div>
+
+  <?php include("index-footer.php")?>
 
 </div>
 
