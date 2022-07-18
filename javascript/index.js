44ec0d0e6cb12c27bbe04e060cd7be55a1bff209
KW <kaiwaing1722@gmail.com>
2022-07-19 00:56:23 +0800

    js

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,22 @@
+/* get data from document & split data to array by "/" */
+let date1 = document.querySelector(".date1").innerHTML;
+let datearray = date1.split("/");
 
-  
+let date2 = document.querySelector(".date2").innerHTML;
+let datearray2 = date2.split("/");
+
+/* adapt MM/DD/YY to DD/MM/YY */
+let newdatearray = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+let newdatearray2 = datearray2[1] + '/' + datearray2[0] + '/' + datearray2[2];
+
+/* convert string to date */
+let diffdate1 = new Date (newdatearray);
+let diffdate2 = new Date (newdatearray2);
+
+/* calculate the diff */
+let Difference_In_Time = diffdate2.getTime() - diffdate1.getTime();
+let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
 
-
-
-let date1=document.querySelector(".date1").innerHTML;
-let datearray=date1.split("/");
-
-let newdatearray=datearray[1] + '/' + datearray[0] + '/' + datearray[2];
-
-
-  
-let date2=document.querySelector(".date2").innerHTML;
-
-let date2array=date2.split("/");
-
-let newdate2array=date2array[1] + '/' + date2array[0] + '/' + date2array[2];
-console.log(newdatearray, newdate2array);
-
-var diffdate1 = new Date (newdatearray);
-var diffdate2 = new Date (newdate2array);
-console.log(diffdate1,diffdate2);
-  
-
-var Difference_In_Time = diffdate2.getTime() - diffdate1.getTime();
-  
-
-var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-  
-
-console.log("Total number of days between dates  <br>"
-               + date1 + "<br> and <br>" 
-               + date2 + " is: <br> " 
-               + Difference_In_Days);
+console.log(newdatearray, newdatearray2); 
+console.log(Difference_In_Days);
