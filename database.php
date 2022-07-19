4f94e5add5ea9f33d4867f29dd6991c716b3c5fa
Joe-KW <kaiwaing1722@gmail.com>
2022-07-19 09:59:58 +0800

    remotesql

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-$dbConnection = mysqli_connect("localhost","root","","green_rental_project");
-// $dbConnection = mysqli_connect("sql308.epizy.com","epiz_32190800","Ial36MlWn4V","epiz_32190800_green_rental_project");
+// $dbConnection = mysqli_connect("localhost","root","","green_rental_project");
+$dbConnection = mysqli_connect("remotemysql.com","mY7D8u4FAO","bj4MWMsIj2","mY7D8u4FAO");
 if(mysqli_connect_errno()){
     echo 'Failed to connect to MySQL';
     exit();
