6bbddd594bc1f8b6a15a77c30fa6e2f2e14d2e63
KW <kaiwaing1722@gmail.com>
2022-07-20 00:18:50 +0800

    change db passord

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-$dbConnection = mysqli_connect("localhost","root","","green_rental_project");
-// $dbConnection = mysqli_connect("remotemysql.com","mY7D8u4FAO","bj4MWMsIj2","mY7D8u4FAO");
+// $dbConnection = mysqli_connect("localhost","root","","green_rental_project");
+$dbConnection = mysqli_connect("remotemysql.com","mY7D8u4FAO","bj4MWMsIj2","mY7D8u4FAO");
 if(mysqli_connect_errno()){
     echo 'Failed to connect to MySQL';
     exit();
