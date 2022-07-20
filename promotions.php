232d353689a4eccdc67d7a3d61a7a09b4f54457a
erbho <erbho@pm.me>
2022-07-20 10:35:09 +0800

    added bootstrap script link to bottom of promo

--- promotions.php
+++ promotions.php
@@ -86,7 +86,7 @@
         </button>
       
       </div>
-      
+
 <br><br>
 <div id="promo1">
   <div class="container justify-content-center w-75" style="background-color: rgb(218, 220, 220); border-radius: 15px;
@@ -149,5 +149,8 @@
   </div>
 </div>
 </div><br><br>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
 <?php include('index-footer.php')?>
