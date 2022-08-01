aca1b1647e63a9ad4674a74bcb069420a9dd4425
EugenePong <Chun_pong0914@hotmail.com>
2022-08-01 15:00:44 +0800

    Mapping finished

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -1,5 +1,19 @@
 let L1=document.querySelector('.locationcheck1').innerHTML;
 let L2=document.querySelector('.locationcheck2').innerHTML;
-let M1=document.querySelector('.showmap');
-console.log(L1,L2,M1)
+let M1=document.querySelector('.showmap1');
+let M2=document.querySelector('.showmap2');
+console.log(L1,L2,M1,M2)
 
+if (L1=="觀塘分店"){
+    M1.setAttribute("href", "https://goo.gl/maps/D1bYPYAGmVsz7H3AA");
+}
+else{
+    M1.setAttribute("href", "https://goo.gl/maps/hMskNwCdmrrGaPy19");
+}
+
+if (L2=="上水分店"){
+    M2.setAttribute("href", "https://goo.gl/maps/hMskNwCdmrrGaPy19");
+}
+else{
+    M2.setAttribute("href", "https://goo.gl/maps/D1bYPYAGmVsz7H3AA");
+}
