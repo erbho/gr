b037588bb00b3adcaf5c10cad2b5402787103aa9
erbho <erbho@proton.me>
2022-08-05 04:19:03 +0800

    index js

--- javascript/index.js
+++ javascript/index.js
@@ -55,11 +55,11 @@ localStorage.rentFee = rentFee;
 localStorage.total = total.toLocaleString();
 
 
-// document.querySelector(".pickupDateTime").innerHTML = localStorage.pickuptimeDate;
-// document.querySelector(".pickupBranch").innerHTML = localStorage.pickupBranch;
+document.querySelector(".pickupDateTime").innerHTML = localStorage.pickuptimeDate;
+document.querySelector(".pickupBranch").innerHTML = localStorage.pickupBranch;
 
-// document.querySelector(".returnDateTime").innerHTML = localStorage.returntimeDate;
-// document.querySelector(".returnBranch").innerHTML = localStorage.returnBranch;
+document.querySelector(".returnDateTime").innerHTML = localStorage.returntimeDate;
+document.querySelector(".returnBranch").innerHTML = localStorage.returnBranch;
 
 /* input day & total price for db */
 document.querySelector('.getrentDayValue').value = rentDay;
