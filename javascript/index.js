c65c33c7520d5ba7eec1b73ea89065ac23fbfa3f
erbho <erbho@proton.me>
2022-08-05 13:57:41 +0800

    .

--- javascript/index.js
+++ javascript/index.js
@@ -1,44 +1,3 @@
-/* get data from document & split data to array by "/" */
-// let date1 = document.querySelector(".date1").value;
-// console.log(date1);
-// // let datearray = date1.split("/");
-
-// let date2 = document.querySelector(".date2").value;
-// console.log(date2);
-// // let datearray2 = date2.split("/");
-
-// // /* adapt MM/DD/YY to DD/MM/YY */
-// // let newdatearray = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
-// // let newdatearray2 = datearray2[1] + '/' + datearray2[0] + '/' + datearray2[2];
-
-// /* convert string to date */
-// let diffdate1 = new Date (date1);
-// let diffdate2 = new Date (date2);
-
-// /* calculate the diff */
-// let Difference_In_Time = diffdate2.getTime() - diffdate1.getTime();
-// let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-// let A = Math.ceil (Difference_In_Days)
-
-
-// / console.log(newdatearray, newdatearray2); 
-// console.log(A);
-
-
-
-
-// let date1=document.querySelector(".date1").value;
-// let date2=document.querySelector(".date2").value;
-
-// let diffdate1 = new Date (date1)
-// let diffdate2 = new Date(date2)
-
-// let time_between = diffdate2.getTime() - diffdate1.getTime();
-// let day_between = time_between / (1000*3600*24);
-// let dayceil_between = Math.ceil(day_between);
-
-
-
 document.querySelector(".rentDay").innerHTML = JSON.parse(localStorage.rentDay);
 
 const rentFee = Number(document.querySelector(".rentFee").innerHTML);
@@ -62,16 +21,16 @@ minDate = new Date(today.getTime() - today.getTimezoneOffset() * 60000).toISOStr
 document.querySelector(".pickuptime").setAttribute("min", minDate);
 document.querySelector(".returntime").setAttribute("min", minDate);
 
-let maxPickup = document.querySelector('.returntime');
-maxPickup.addEventListener('change',setMaxPickupDate);
+let maxPickup = document.querySelector(".returntime");
+maxPickup.addEventListener("change",setMaxPickupDate);
 
 function setMaxPickupDate () {
     let returntime = document.querySelector(".returntime").value;
     document.querySelector(".pickuptime").setAttribute("max", returntime);
 }
 
-let minReturn = document.querySelector('.pickuptime');
-minReturn.addEventListener('change',setMinReturnDate);
+let minReturn = document.querySelector(".pickuptime");
+minReturn.addEventListener("change",setMinReturnDate);
 
 function setMinReturnDate () {
     let pickuptime = document.querySelector(".pickuptime").value;
@@ -80,6 +39,6 @@ function setMinReturnDate () {
 
 
 /* input day & total price for db */
-document.querySelector('.getrentDayValue').value = rentDay;
-document.querySelector('.getTotalPriceValue').value = document.querySelector(".totalPrice").innerHTML;
+document.querySelector(".getrentDayValue").value = rentDay;
+document.querySelector(".getTotalPriceValue").value = document.querySelector(".totalPrice").innerHTML;
 
