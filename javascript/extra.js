71722e3209876ca62cf440b196a31dd41283955a
erbho <erbho@proton.me>
2022-08-05 14:09:34 +0800

    .

--- javascript/extra.js
+++ javascript/extra.js
@@ -26,19 +26,20 @@ let rtTimeLength = rtTime.length;
 let rtHour = rtTime.slice(0,(rtTimeLength-6));
 let rtAMPM = rtTime.slice(-2);
 
-if (pickuptime=="" || returntime=="") {
-    alert("請選取時間");
-    btnCkeck.setAttribute('type','button');
-    
-} else if (numberOfPickuptime>numberOfReturntime || numberOfPickuptime==numberOfReturntime) {
-    alert("錯誤! 請選擇正確時間");
-    btnCkeck.setAttribute('type','button');
-} else {
-    btnCkeck.setAttribute('type','submit');
-    localStorage.clear();
-    localStorage.pickuptimeDate = puDate + ", " + puHour + " " + puAMPM;
-    localStorage.returntimeDate = rtDate + ", " + rtHour + " " + rtAMPM;
-    localStorage.rentDay = Math.ceil((numberOfReturntime - numberOfPickuptime)/(1000*3600*24));
-    localStorage.pickupBranch = document.querySelector("#pickupBranch").value;
-    localStorage.returnBranch = document.querySelector("#returnBranch").value; 
+    if (pickuptime=="" || returntime=="") {
+        alert("請選取時間");
+        btnCkeck.setAttribute('type','button');
+        
+    } else if (numberOfPickuptime>numberOfReturntime || numberOfPickuptime==numberOfReturntime) {
+        alert("錯誤! 請選擇正確時間");
+        btnCkeck.setAttribute('type','button');
+    } else {
+        btnCkeck.setAttribute('type','submit');
+        localStorage.clear();
+        localStorage.pickuptimeDate = puDate + ", " + puHour + " " + puAMPM;
+        localStorage.returntimeDate = rtDate + ", " + rtHour + " " + rtAMPM;
+        localStorage.rentDay = Math.ceil((numberOfReturntime - numberOfPickuptime)/(1000*3600*24));
+        localStorage.pickupBranch = document.querySelector("#pickupBranch").value;
+        localStorage.returnBranch = document.querySelector("#returnBranch").value; 
+    }
 }
